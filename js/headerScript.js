8bca2f3db9364cdc4c9be5488ec07cd7a7c972c9
Bankmock <133338375+Bankmock@users.noreply.github.com>
2025-01-23 10:02:06 +0530

    Update headerScript.js

--- js/headerScript.js
+++ js/headerScript.js
@@ -1,7 +1,1 @@
-var script = document.createElement('script');
-script.type = 'text/javascript';
-script.src = 'https://platform.foremedia.net/code/58760/analytics';
-script.async = true;
-document.head.appendChild(script); 
 
-alert("hi");
