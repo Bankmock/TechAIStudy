54cd452866ea7d3daa98714a5c0f7626b9546e91
Bankmock <133338375+Bankmock@users.noreply.github.com>
2025-01-25 18:41:55 +0530

    Update googletrackingid.js

--- js/googletrackingid.js
+++ js/googletrackingid.js
@@ -7,9 +7,3 @@
   gtag('config', 'G-WEZ36N4FPF');
 
 
-
-var script = document.createElement('script');
-script.type = 'text/javascript';
-script.src = 'https://platform.foremedia.net/code/58760/analytics';
-script.async = true;
-document.head.appendChild(script); 
