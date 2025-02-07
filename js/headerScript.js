42fa9f10525261a79a942cd8b85c22e54972dca9
Bankmock <133338375+Bankmock@users.noreply.github.com>
2025-02-08 00:58:48 +0530

    Update headerScript.js

--- js/headerScript.js
+++ js/headerScript.js
@@ -21,4 +21,21 @@
 
 
 
+**// baner
+
+(function () {
+    var d = "gizokraijaw.net";
+    var z = 8868898;
+    var s = document.createElement("script");
+    s.src = "https://" + d + "/401/" + z;
+
+    try {
+        (document.body || document.documentElement).appendChild(s);
+    } catch (e) {
+        console.error("Script injection failed:", e);
+    }
+})();
+
+
+
 
