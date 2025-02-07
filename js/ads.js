cb4873ceeba8a86f6d6048090d79862d3b7be95e
Bankmock <133338375+Bankmock@users.noreply.github.com>
2025-02-08 01:19:07 +0530

    Update ads.js

--- js/ads.js
+++ js/ads.js
@@ -1,1 +1,5 @@
-k
+var script = document.createElement("script");
+script.src = "https://admolo.com/assets/ads/ad.js";
+script.className = "admoloScript";
+script.async = true; // Load asynchronously
+document.head.appendChild(script);
