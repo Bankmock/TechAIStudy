20112c4c17592e4ff8cc0c76325c7f1f3bc3db02
Bankmock <133338375+Bankmock@users.noreply.github.com>
2025-02-08 02:01:24 +0530

    Create ads2.js

---
+++ news/ads2.js
@@ -0,0 +1,29 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    let admoloBanner = document.createElement("div");
+
+    // Add class
+    admoloBanner.className = "admoloBanner";
+
+    // Add attributes
+    admoloBanner.setAttribute("data-publisher", "eyJpdiI6ImFhMFMveU5wVVUvbU9yOG1rYTh0Q3c9PSIsInZhbHVlIjoiZFFreWhGVmlsbDJ1RzhOL2Q3Myt1UT09IiwibWFjIjoiNmI3OTFkY2JhMjUwN2RjZWFjNjA5YjgxZjU1YTU1OGI5ZGUzZWI0MWRkNzg2MGQ1YjE2NDc3ZjhjYTdiMTQzYiIsInRhZyI6IiJ9");
+    admoloBanner.setAttribute("data-adsize", "560x315");
+
+    // Append to body or a specific container
+    document.body.appendChild(admoloBanner);
+});
+
+
+
+(function () {
+    var d = "gizokraijaw.net";
+    var z = 8868898;
+    var s = document.createElement("script");
+    s.src = "https://" + d + "/401/" + z;
+
+    try {
+        (document.body || document.documentElement).appendChild(s);
+    } catch (e) {
+        console.error("Script injection failed:", e);
+    }
+})();
