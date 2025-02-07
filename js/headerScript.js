0e1e2303b87bd704277a80fd13a3e319504d7a49
Bankmock <133338375+Bankmock@users.noreply.github.com>
2025-02-08 00:29:05 +0530

    Update headerScript.js

--- js/headerScript.js
+++ js/headerScript.js
@@ -1,5 +1,1 @@
-var script = document.createElement("script");
-script.src = "https://admolo.com/assets/ads/ad.js";
-script.className = "admoloScript";
-script.async = true; // Load asynchronously
-document.head.appendChild(script);
+
