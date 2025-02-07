c9aa1db886b0b936e33d5034d4e8c95e1ad3b1a0
Bankmock <133338375+Bankmock@users.noreply.github.com>
2025-02-08 04:13:11 +0530

    Update ads1.js

--- news/ads1.js
+++ news/ads1.js
@@ -1,17 +1,4 @@
 
-document.addEventListener("DOMContentLoaded", function () {
-    let admoloBanner = document.createElement("div");
-
-    // Add class
-    admoloBanner.className = "admoloBanner";
-
-    // Add attributes
-    admoloBanner.setAttribute("data-publisher", "eyJpdiI6InRKZVVNakhWRHV4bkdTTVpVaU5Pbnc9PSIsInZhbHVlIjoicGN0VGN6VDlYK1o2Q1RJTCt3elo5dz09IiwibWFjIjoiOTgwZDk5MjlkYTA2OTg5ODYwZWNkMGM2ZDg5YmI5YjFmYjRhOTY4OWUxZTZhOGZjZGE1ZWE4MjZlNWU3NmYwMyIsInRhZyI6IiJ9");
-    admoloBanner.setAttribute("data-adsize", "300x300");
-
-    // Append to body or a specific container
-    document.body.appendChild(admoloBanner);
-});
 
 
 
