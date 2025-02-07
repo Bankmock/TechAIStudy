280b33003d8a76c94d2b15afe90c5206f0737c6a
Bankmock <133338375+Bankmock@users.noreply.github.com>
2025-02-08 02:01:50 +0530

    Create ads3.js

---
+++ news/ads3.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let admoloBanner = document.createElement("div");
+
+    // Add class
+    admoloBanner.className = "admoloBanner";
+
+    // Add attributes
+    admoloBanner.setAttribute("data-publisher", "eyJpdiI6ImFXU3lCeXJBMlZTMEtHamdBcW9ndmc9PSIsInZhbHVlIjoiN1IzUThlTiszc3FiSkVBNjlkK0lMdz09IiwibWFjIjoiNDBlMTJiYzRjNzk4MjEwMTExMzM4YzQyMTg1MmY5YTAzZmUzYzVlMjVmNjUwYTgyMmUwMGE4MzZhMmQ2ODFhYyIsInRhZyI6IiJ9");
+    admoloBanner.setAttribute("data-adsize", "320x180");
+
+    // Append to body or a specific container
+    document.body.appendChild(admoloBanner);
+});
