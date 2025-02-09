0f61dcb330b5cf7a1bc8b36df76358481cc73674
Bankmock <133338375+Bankmock@users.noreply.github.com>
2025-02-10 01:27:34 +0530

    Update ads2.js

--- news/ads2.js
+++ news/ads2.js
@@ -15,15 +15,3 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-(function () {
-    var d = "gizokraijaw.net";
-    var z = 8868898;
-    var s = document.createElement("script");
-    s.src = "https://" + d + "/401/" + z;
-
-    try {
-        (document.body || document.documentElement).appendChild(s);
-    } catch (e) {
-        console.error("Script injection failed:", e);
-    }
-})();
