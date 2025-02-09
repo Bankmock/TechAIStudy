057aa276c4c553041a658557d5b804e27a3265b8
Bankmock <133338375+Bankmock@users.noreply.github.com>
2025-02-10 01:32:58 +0530

    Update

---
+++ ads1.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let adContainer = document.createElement("div");
+    adContainer.className = "admoloBanner";
+    adContainer.setAttribute("data-publisher", "eyJpdiI6InRKZVVNakhWRHV4bkdTTVpVaU5Pbnc9PSIsInZhbHVlIjoicGN0VGN6VDlYK1o2Q1RJTCt3elo5dz09IiwibWFjIjoiOTgwZDk5MjlkYTA2OTg5ODYwZWNkMGM2ZDg5YmI5YjFmYjRhOTY4OWUxZTZhOGZjZGE1ZWE4MjZlNWU3NmYwMyIsInRhZyI6IiJ9");
+    adContainer.setAttribute("data-adsize", "300x300");
+
+    document.body.appendChild(adContainer); // Body में Ad जोड़ें
+});
