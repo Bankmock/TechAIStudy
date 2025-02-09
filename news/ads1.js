55dde9749d9a9974ee6524255974ae470efa0fd7
Bankmock <133338375+Bankmock@users.noreply.github.com>
2025-02-10 01:26:55 +0530

    Update ads1.js

--- news/ads1.js
+++ news/ads1.js
@@ -1,25 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let adContainer = document.createElement("div");
+    adContainer.className = "admoloBanner";
+    adContainer.setAttribute("data-publisher", "eyJpdiI6InRKZVVNakhWRHV4bkdTTVpVaU5Pbnc9PSIsInZhbHVlIjoicGN0VGN6VDlYK1o2Q1RJTCt3elo5dz09IiwibWFjIjoiOTgwZDk5MjlkYTA2OTg5ODYwZWNkMGM2ZDg5YmI5YjFmYjRhOTY4OWUxZTZhOGZjZGE1ZWE4MjZlNWU3NmYwMyIsInRhZyI6IiJ9");
+    adContainer.setAttribute("data-adsize", "300x300");
 
-
-
-
-
-(function () {
-    var d = 'groleegni.net';
-    var z = 8868889;
-    var s = document.createElement('script');
-    s.src = 'https://' + d + '/401/' + z;
-    
-    s.onload = function() {
-        console.log('Script loaded successfully.');
-    };
-
-    s.onerror = function() {
-        console.error('Failed to load script.');
-    };
-
-    try {
-        (document.body || document.documentElement).appendChild(s);
-    } catch (e) {
-        console.error('Error appending script:', e);
-    }
-})();
+    document.body.appendChild(adContainer); // Body में Ad जोड़ें
+});
