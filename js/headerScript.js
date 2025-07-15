83271ba502505b7e4f8965cf84e8eefc994fe2ee
Bankmock <133338375+Bankmock@users.noreply.github.com>
2025-07-15 22:09:17 +0530

    Update headerScript.js

--- js/headerScript.js
+++ js/headerScript.js
@@ -1,9 +1,3 @@
-(function() {
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = '//pl25250855.profitableratecpm.com/2f/45/0d/2f450dbe2d59996e21cf849955b401b9.js';
-  document.head.appendChild(script);
-})();
 
 
 function toggleTheme() {
@@ -18,37 +12,4 @@ function toggleTheme() {
       dropdown.classList.toggle('show');
     }
   
-// Create the script tag dynamically
-var admoloScript = document.createElement('script');
-admoloScript.className = 'admoloScript';
-admoloScript.src = 'https://admolo.com/assets/ads/ad.js';
-admoloScript.async = true; // optional, helps non-blocking load
-
-// Append it to the <head> or <body>
-document.head.appendChild(admoloScript);
-// or use: document.body.appendChild(admoloScript);
-
-
-
-// Define montag variables
-var domain = 'groleegni.net';
-var zoneId = 9019209;
-
-// Create a script element
-var script = document.createElement('script');
-
-// Set the script source
-script.src = 'https://' + domain + '/401/' + zoneId;
-
-try {
-  // Try appending the script to the body or documentElement
-  (document.body || document.documentElement).appendChild(script);
-} catch (e) {
-  // Fail silently
-}
-
-
-
-
-
 
