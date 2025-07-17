9a6591f83fa8e2914ceb38fa355980d0569eb1d6
Bankmock <133338375+Bankmock@users.noreply.github.com>
2025-07-17 21:35:32 +0530

    Update ads.js

--- js/ads.js
+++ js/ads.js
@@ -1,1 +1,28 @@
-google.com, pub-7020673744407070, DIRECT, f08c47fec0942fa0
+// In-Page Ads
+(function() {
+  const script1 = document.createElement('script');
+  script1.src = 'https://richinfo.co/richpartners/in-page/js/richads-ob.js?pubid=952603&siteid=355035';
+  script1.async = true;
+  document.head.appendChild(script1);
+})();
+
+// Push Ads (module script)
+(function() {
+  const script2 = document.createElement('script');
+  script2.src = 'https://richinfo.co/richpartners/push/js/rp-cl-ob.js?pubid=952603&siteid=355037&niche=33';
+  script2.type = 'module';
+  script2.async = true;
+  script2.setAttribute('data-cfasync', 'false');
+  document.head.appendChild(script2);
+})();
+
+// Popunder Ads
+(function() {
+  const script3 = document.createElement('script');
+  script3.src = 'https://richinfo.co/richpartners/pops/js/richads-pu-ob.js';
+  script3.async = true;
+  script3.setAttribute('data-pubid', '952603');
+  script3.setAttribute('data-siteid', '357344');
+  script3.setAttribute('data-cfasync', 'false');
+  document.head.appendChild(script3);
+})();
