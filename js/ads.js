77bad05cd2d2ad3caa2dd3f0a54ecb9534853cb5
Bankmock <133338375+Bankmock@users.noreply.github.com>
2025-07-22 15:18:11 +0530

    Update ads.js

---
+++ js/ads.js
@@ -0,0 +1,8 @@
+(function() {
+  var s = document.createElement('script');
+  s.src = 'https://fpyf8.com/88/tag.min.js';
+  s.setAttribute('data-zone', '158684');
+  s.async = true;
+  s.setAttribute('data-cfasync', 'false');
+  document.head.appendChild(s);
+})();
