1c28d0a673de6de8d7e4962194e60ea1f2744cfe
Bankmock <133338375+Bankmock@users.noreply.github.com>
2025-07-24 00:53:21 +0530

    Update ads.js

--- js/ads.js
+++ js/ads.js
@@ -1,22 +1,40 @@
+(function () {
+  // Wait for DOM to be ready
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', injectAd);
+  } else {
+    injectAd();
+  }
 
-(function() {
-  // Load the GPT.js script dynamically
-  var script = document.createElement('script');
-  script.src = "https://securepubads.shareus.io/scripts/tag/js/gpt.js";
-  script.async = true;
-  script.type = "text/javascript";
+  function injectAd() {
+    // Create container div
+    var adDiv = document.createElement('div');
+    adDiv.id = 'shareus-admanager-0530423619-B47xbLLveQ';
 
-  script.onload = function() {
-    window._shareustag = window._shareustag || { cmd: [] };
+    // Insert just after <body> tag
+    if (document.body.firstChild) {
+      document.body.insertBefore(adDiv, document.body.firstChild);
+    } else {
+      document.body.appendChild(adDiv);
+    }
 
-    _shareustag.cmd.push(function() {
-      _shareustag.defineSlot(
-        '/shareus-admanager/0530423619/B47xbLLveQ/K',
-        ["Responsive"],
-        'shareus-admanager-0530423619-B47xbLLveQ'
-      );
-    });
-  };
+    // Load Shareus script
+    var script = document.createElement('script');
+    script.src = 'https://securepubads.shareus.io/scripts/tag/js/gpt.js';
+    script.async = true;
+    script.type = 'text/javascript';
 
-  document.head.appendChild(script);
+    script.onload = function () {
+      window._shareustag = window._shareustag || { cmd: [] };
+      _shareustag.cmd.push(function () {
+        _shareustag.defineSlot(
+          '/shareus-admanager/0530423619/B47xbLLveQ/K',
+          ['Responsive'],
+          'shareus-admanager-0530423619-B47xbLLveQ'
+        );
+      });
+    };
+
+    document.head.appendChild(script);
+  }
 })();
