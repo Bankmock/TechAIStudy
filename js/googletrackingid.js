e4606167592ad7632351ee4f61968e74a3d5b82b
Bankmock <133338375+Bankmock@users.noreply.github.com>
2025-07-25 10:37:17 +0530

    Update googletrackingid.js

--- js/googletrackingid.js
+++ js/googletrackingid.js
@@ -8,17 +8,6 @@
 
 
 
-(function() {
-  var metaTagName = "google-adsense-account";
-  var existing = document.querySelector('meta[name="' + metaTagName + '"]');
-  if (!existing) {
-    var meta = document.createElement("meta");
-    meta.name = metaTagName;
-    meta.content = "ca-pub-7020673744407070";
-    document.head.appendChild(meta);
-  }
-})();
-
 
 
 
