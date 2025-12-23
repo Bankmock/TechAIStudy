f61733326da1abe3ab90884dc6ba07524700aca5
Bankmock <133338375+Bankmock@users.noreply.github.com>
2025-12-23 20:20:03 +0530

    Update ads.js

--- js/ads.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  if (document.getElementById('egcpm-injected')) return;
-
-  var s = document.createElement('script');
-  s.id = 'egcpm-injected';
-  s.src = 'https://pl27161817.effectivegatecpm.com/72/22/73/722273486ea0084fcc66884a230db852.js';
-  s.async = true;
-  document.head.appendChild(s);
-})();
